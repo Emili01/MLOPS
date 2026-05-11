e9a57036ec9ed5b14d1a8c112087755e2aafb95d
Emilio Muñoz Rivera <123415921+Emili01@users.noreply.github.com>
2026-05-11 10:51:21 -0600

    Add files via upload

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,14 +1,8 @@
 resource "aws_s3_bucket" "datalake" {
   bucket        = var.s3_bucket_name
-  force_destroy = true  # Permitir eliminación aunque tenga objetos
+  force_destroy = true 
 }
 
-# Eliminar estos recursos inicialmente:
-# resource "aws_s3_object" "raw_folder" { ... }
-# resource "aws_s3_object" "silver_folder" { ... }
-# resource "aws_s3_object" "models_folder" { ... }
-
-# Usar este recurso alternativo para crear las carpetas
 resource "terraform_data" "create_s3_folders" {
   depends_on = [aws_s3_bucket.datalake]
   
